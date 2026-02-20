35637c48a8e722bf9a65921625cbe5b54a7b3586
Anger Armand <aanger3@gmail.com>
2026-02-20 09:32:01 +0100

    fix : mem alignement

--- ft_strdup.s
+++ ft_strdup.s
@@ -6,28 +6,34 @@ section .text
     global ft_strdup
 
 ; char *strdup(const char *s);
-; rax  *strduo(rdi)
+; rax  *strdup(rdi)
 ft_strdup:
-    call ft_strlen ;rdi already contain the *char
+    call ft_strlen
 
     ; if len == 0
     cmp rax, 0
     je error
 
-    push rdi    ; save char *s
+    push rdi                ; save char *s
     mov r9, rax
-    push r9     ; save len
+    push r9                 ; save len
 
-    inc rax ; len + 1 = '\0' -> for alloc size
+    inc rax                 ; len + 1 = '\0' -> for alloc size
     mov rdi, rax
+
+    mov r12, rsp            ; Save the stack pointer
+    and rsp, -16            ; Align rsp before call libc
     call malloc wrt ..plt
-    cmp rax, 0 ; if malloc return NULL
+    mov rsp, r12            ; Restore rsp after call
+
+    cmp rax, 0              ; if malloc return NULL
     je error
 
-    pop r9
-    pop rdi     ; get char *s
 
-    xor rcx, rcx ; counter -> set rcx = 0
+    pop r9                  ; get len
+    pop rdi                 ; get char *s
+
+    xor rcx, rcx            ; counter -> set rcx = 0
 
 loop:
 
